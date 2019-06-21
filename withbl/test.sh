a9dd0f3ad0b3c7e0a9b085568f832f91e1699e57
D0ot <1306793135@qq.com>
2019-06-21 11:46:23 +0800

    delete confirm;

--- withbl/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-qemu-system-i386 -kernel myos.bin -m 128 -monitor stdio -gdb tcp::1234 
--- withoutbl/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-qemu-system-i386 -hda boot.bin -gdb tcp::1234 -S -monitor stdio
